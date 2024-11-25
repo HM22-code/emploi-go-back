a710d8de178edbab96888a0d6625e7f94d7f172a
HM22-code <H.M-22@outlook.fr>
2024-11-25 23:18:19 +0100

    Adding dependencies

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize');
+
+// Environment variables can be used to set the database dynamically
+const DB_NAME = process.env.DB_NAME || 'oauth_db';
+const DB_USER = process.env.DB_USER || 'root';
+const DB_PASSWORD = process.env.DB_PASSWORD || null;
+const DB_DIALECT = process.env.DB_DIALECT || 'sqlite';
+const DB_STORAGE = process.env.DB_STORAGE || './database.sqlite';
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: process.env.DB_HOST || 'localhost',
+  dialect: DB_DIALECT,
+  storage: DB_DIALECT === 'sqlite' ? DB_STORAGE : undefined,
+});
+
+module.exports = sequelize;
---
+++ models/user.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    unique: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+}, {});
+
+module.exports = User;
