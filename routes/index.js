c83f12d0c5356142663dab9d15a344c4127a1955
HM22-code <H.M-22@outlook.fr>
2024-11-25 22:13:36 +0100

    Adding forms views

--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+/* GET logout page. */
+router.get('/logout', function(req, res, next) {
+  res.render('logout');
+});
+
+/* GET signup page. */
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
 module.exports = router;
