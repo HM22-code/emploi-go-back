b370fb4235dafb6e346b4bf8bd166e7704fd4472
HM22-code <H.M-22@outlook.fr>
2024-11-28 11:15:50 +0100

    Adding role property to user

--- models/user.js
+++ models/user.js
@@ -22,6 +22,10 @@ const User = sequelize.define('User', {
     type: DataTypes.STRING,
     allowNull: false,
   },
+  role: {
+    type: DataTypes.ENUM('USER', 'ADMIN'),
+    defaultValue: 'USER',
+  },
 }, {
   timestamps: false
 });
